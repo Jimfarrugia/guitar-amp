09c6786dd06b7d19fdde11b5df69b39021f8dd9b
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-03-01 03:54:58 +1100

    setup visualizer

--- script.js
+++ script.js
@@ -5,8 +5,18 @@ const treble = document.getElementById('treble');
 const visualizer = document.getElementById('visualizer');
 
 const context = new AudioContext();
+const analyserNode = new AnalyserNode(context, { fftSize: 256 });
 
 setupContext();
+drawVisualizer();
+resize();
+
+function setupEventListeners() {
+	window.addEventListener('resize', resize);
+	volume.addEventListener('input', e => {
+		const value = parseFloat(e.target.value);
+	});
+}
 
 async function setupContext() {
 	const guitar = await getGuitar();
@@ -14,8 +24,10 @@ async function setupContext() {
 		await context.resume();
 	}
 	const source = context.createMediaStreamSource(guitar);
-	source.connect(context.destination);
-}
+	source
+		.connect(analyserNode)
+		.connect(context.destination);
+};
 
 function getGuitar() {
 	return navigator.mediaDevices.getUserMedia({
@@ -26,4 +38,31 @@ function getGuitar() {
 			latency: 0
 		}
 	});
-}
+};
+
+function drawVisualizer() {
+	requestAnimationFrame(drawVisualizer);
+
+	const bufferLength = analyserNode.frequencyBinCount;
+	const dataArray = new Uint8Array(bufferLength);
+	analyserNode.getByteFrequencyData(dataArray);
+	const width = visualizer.width;
+	const height = visualizer.height;
+	const barWidth = width / bufferLength;
+
+	const canvasContext = visualizer.getContext('2d');
+	canvasContext.clearRect(0, 0, width, height);
+
+	dataArray.forEach((item, index) => {
+		const y = item / 255 * height / 2;
+		const x = barWidth * index;
+		canvasContext.fillStyle = `hsl(${y / height * 400}, 100%, 50%)`;
+		canvasContext.fillRect(x, height - y, barWidth, y);
+	});
+};
+
+function resize() {
+	visualizer.width = visualizer.clientWidth * window.devicePixelRatio;
+	visualizer.height = visualizer.clientHeight * window.devicePixelRatio;
+
+};
