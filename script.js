059a151bdd7fb6fbef6f0ae2b424cb9631e07ee9
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-03-01 04:11:38 +1100

    setup EQ controls

--- script.js
+++ script.js
@@ -6,15 +6,50 @@ const visualizer = document.getElementById('visualizer');
 
 const context = new AudioContext();
 const analyserNode = new AnalyserNode(context, { fftSize: 256 });
+const gainNode = new GainNode(context, { gain: volume.value });
+const bassEQ = new BiquadFilterNode(context, {
+	type: 'lowshelf',
+	frequency: 500,
+	gain: bass.value
+});
+const midEQ = new BiquadFilterNode(context, {
+	type: 'peaking',
+	Q: Math.SQRT1_2,
+	frequency: 1500,
+	gain: mid.value
+});
+const trebleEQ = new BiquadFilterNode(context, {
+	type: 'highshelf',
+	frequency: 3000,
+	gain: treble.value
+});
 
+setupEventListeners();
 setupContext();
 drawVisualizer();
 resize();
 
 function setupEventListeners() {
 	window.addEventListener('resize', resize);
+
 	volume.addEventListener('input', e => {
 		const value = parseFloat(e.target.value);
+		gainNode.gain.setTargetAtTime(value, context.currentTime, 0.01);
+	});
+
+	bass.addEventListener('input', e => {
+		const value = parseInt(e.target.value);
+		bassEQ.gain.setTargetAtTime(value, context.currentTime, 0.01);
+	});
+
+	mid.addEventListener('input', e => {
+		const value = parseInt(e.target.value);
+		midEQ.gain.setTargetAtTime(value, context.currentTime, 0.01);
+	});
+
+	treble.addEventListener('input', e => {
+		const value = parseInt(e.target.value);
+		trebleEQ.gain.setTargetAtTime(value, context.currentTime, 0.01);
 	});
 }
 
@@ -25,6 +60,10 @@ async function setupContext() {
 	}
 	const source = context.createMediaStreamSource(guitar);
 	source
+		.connect(bassEQ)
+		.connect(midEQ)
+		.connect(trebleEQ)
+		.connect(gainNode)
 		.connect(analyserNode)
 		.connect(context.destination);
 };
@@ -64,5 +103,4 @@ function drawVisualizer() {
 function resize() {
 	visualizer.width = visualizer.clientWidth * window.devicePixelRatio;
 	visualizer.height = visualizer.clientHeight * window.devicePixelRatio;
-
 };
